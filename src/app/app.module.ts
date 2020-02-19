e2c00e81f4ef6051c487c370df7925c2a0322d87
Mark Scott-Kiddie <mwscottkiddie@gmail.com>
2020-02-19 16:29:55 +0000

    added some useless functionality but stying

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,30 @@ import { NgModule } from '@angular/core';
 import {
   MatToolbarModule,
   MatIconModule,
-  MatButtonModule, } from '@angular/material';
+  MatButtonModule,
+  MatCardModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HeaderComponent } from './header/header.component';
 import { EditCvComponent } from './edit-cv/edit-cv.component';
+import { CvComponent } from './cv/cv.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    EditCvComponent
+    EditCvComponent,
+    CvComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
