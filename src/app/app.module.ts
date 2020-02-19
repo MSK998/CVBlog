47ce1f6afa65903b767fdcf93eab1a12618081f9
Mark Scott-Kiddie <mwscottkiddie@gmail.com>
2020-02-19 13:29:14 +0000

    added some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatToolbarModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule, } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HeaderComponent } from './header/header.component';
+import { EditCvComponent } from './edit-cv/edit-cv.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    EditCvComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/edit-cv/edit-cv.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-cv',
+  templateUrl: './edit-cv.component.html',
+  styleUrls: ['./edit-cv.component.css']
+})
+
+export class EditCvComponent {
+
+}
